20bb4020ca60cc572e2694b1535057b4819df693
Micah <micah.mutrux@eatingwell.com>
2016-04-05 08:03:34 -0400

    TESTS: initial tests for user model
    Need to continue working on this, but at least I have a working
    test that I made myself..

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,5 +1,16 @@
 require 'rails_helper'
 
+# first factorygirl test following http://everydayrails.com/2012/03/19/testing-series-rspec-models-factory-girl.html
 RSpec.describe User, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+
+  it "has a valid factory" do
+    expect( FactoryGirl.create(:user, name: "micah") ).to be_valid
+  end
+  
+  it "is invalid without a name" do
+    pending "learning stuff" 
+    expect( FactoryGirl.build(:user, name: nil) ).to_not be_valid
+  end
+
+  it "is invalid without an email"
 end
