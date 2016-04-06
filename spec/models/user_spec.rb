62f54f614727cfd84bac2c9b7963321ab8465b88
Micah <micah.mutrux@eatingwell.com>
2016-04-06 09:26:14 -0400

    TESTS: fixed user model tests
    Don't forget, you can't use .create for an instance that you expect
    to fail - use .build instead.

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -2,15 +2,28 @@ require 'rails_helper'
 
 # first factorygirl test following http://everydayrails.com/2012/03/19/testing-series-rspec-models-factory-girl.html
 RSpec.describe User, type: :model do
-
-  it "has a valid factory" do
-    expect( FactoryGirl.create(:user, name: "micah") ).to be_valid
+  
+  before(:each) do
+    @fg = FactoryGirl
   end
   
-  it "is invalid without a name" do
-    pending "learning stuff" 
-    expect( FactoryGirl.build(:user, name: nil) ).to_not be_valid
-  end
+  context "User Properties" do
+  
+    it "has a valid factory" do
+      user = @fg.build(:user) 
+      expect( user ).to be_valid
+    end
+    
+    it "does not require a name" do
+      user_without_name = @fg.build(:user, name: nil) 
+      expect( user_without_name ).to be_valid
+    end
+  
+    it "requires an email" do 
+      user_without_email = @fg.build(:user, email: nil) 
+      expect( user_without_email ).to_not be_valid
+    end
 
-  it "is invalid without an email"
+  end 
+  
 end
