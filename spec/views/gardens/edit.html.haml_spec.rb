fd4825c9fa1f0dd31df9ffcd0b798902836a5ddc
Micah <micah.mutrux@eatingwell.com>
2016-04-07 07:37:10 -0400

    TESTS: fixed broken view tests
    Because I added the association between gardens and users later on,
    the test was still expecting that the user fields would be text
    inputs.  When I changed the assert_select selector to look for a
    'select' field, they passed.  Good to know.

--- spec/views/gardens/edit.html.haml_spec.rb
+++ spec/views/gardens/edit.html.haml_spec.rb
@@ -15,17 +15,17 @@ RSpec.describe "gardens/edit", type: :view do
   it "renders the edit garden form" do
     render
 
-    # assert_select "form[action=?][method=?]", garden_path(@garden), "post" do
-# 
-      # assert_select "input#garden_name[name=?]", "garden[name]"
-# 
-      # assert_select "input#garden_square_feet[name=?]", "garden[square_feet]"
-# 
-      # assert_select "input#garden_zone[name=?]", "garden[zone]"
-# 
-      # assert_select "input#garden_garden_type[name=?]", "garden[garden_type]"
-# 
-      # assert_select "input#garden_user_id[name=?]", "garden[user_id]"
-    # end
+     assert_select "form[action=?][method=?]", garden_path(@garden), "post" do
+ 
+       assert_select "input#garden_name[name=?]", "garden[name]"
+ 
+       assert_select "input#garden_square_feet[name=?]", "garden[square_feet]"
+ 
+       assert_select "input#garden_zone[name=?]", "garden[zone]"
+ 
+       assert_select "input#garden_garden_type[name=?]", "garden[garden_type]"
+ 
+       assert_select "select#garden_user_id[name=?]", "garden[user_id]"
+     end
   end
 end
--- spec/views/gardens/new.html.haml_spec.rb
+++ spec/views/gardens/new.html.haml_spec.rb
@@ -14,17 +14,17 @@ RSpec.describe "gardens/new", type: :view do
   it "renders new garden form" do
     render
 
-    # assert_select "form[action=?][method=?]", gardens_path, "post" do
-# 
-      # assert_select "input#garden_name[name=?]", "garden[name]"
-# 
-      # assert_select "input#garden_square_feet[name=?]", "garden[square_feet]"
-# 
-      # assert_select "input#garden_zone[name=?]", "garden[zone]"
-# 
-      # assert_select "input#garden_garden_type[name=?]", "garden[garden_type]"
-# 
-      # assert_select "input#garden_user_id[name=?]", "garden[user_id]"
-    # end
+     assert_select "form[action=?][method=?]", gardens_path, "post" do
+ 
+       assert_select "input#garden_name[name=?]", "garden[name]"
+ 
+       assert_select "input#garden_square_feet[name=?]", "garden[square_feet]"
+ 
+       assert_select "input#garden_zone[name=?]", "garden[zone]"
+ 
+       assert_select "input#garden_garden_type[name=?]", "garden[garden_type]"
+ 
+       assert_select "select#garden_user_id[name=?]", "garden[user_id]"
+     end
   end
 end
