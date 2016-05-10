8519ab122cc715f7e50b46c5880f8115877725fe
Micah <micah.mutrux@eatingwell.com>
2016-05-10 07:07:34 -0400

    garden: model requires some attributes

--- app/models/garden.rb
+++ app/models/garden.rb
@@ -1,3 +1,10 @@
 class Garden < ActiveRecord::Base
   belongs_to :user
+  
+  validates :square_feet, presence: true
+  validates :name, presence: true
+  validates :zone, presence: true
+  # validates :user, presence: true
+  
+  #TODO: remove user from form and set it to current user in the model
 end
