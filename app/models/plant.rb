6bca7e574dc3a5f55d0275d274087b06e6bb5daa
Micah <micah.mutrux@eatingwell.com>
2016-09-30 06:33:44 -0400

    Plants: created enums for some properties

--- app/models/plant.rb
+++ app/models/plant.rb
@@ -1,3 +1,7 @@
-class Plant < ActiveRecord::Base
+class Plant < ActiveRecord::Base  
   belongs_to :garden
+  
+  enum life_cycle: [ :annual, :perennial, :biennial ]
+  enum sun: [ :full_sun, :part_shade, :full_shade ]
+  enum sow_method: [ :direct, :indoor, :direct_indoor ]
 end
