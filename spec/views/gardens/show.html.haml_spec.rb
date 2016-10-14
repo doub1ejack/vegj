c6c6730f1a81819e3933c3a2955b030bdc7a9c3b
Micah <micah.mutrux@eatingwell.com>
2016-10-14 08:14:06 -0400

    SPEC: use FactoryGirl to get garden:show to pass
    It was having trouble with the previously defined nil user, and
    I updated the matching tests to read from the factory too.

--- spec/views/gardens/show.html.haml_spec.rb
+++ spec/views/gardens/show.html.haml_spec.rb
@@ -2,21 +2,15 @@ require 'rails_helper'
 
 RSpec.describe "gardens/show", type: :view do
   before(:each) do
-    @garden = assign(:garden, Garden.create!(
-      :name => "Name",
-      :square_feet => 1,
-      :zone => 2,
-      :garden_type => "Garden Type",
-      :user => nil
-    ))
+    @garden = assign(:garden, FactoryGirl.create(:garden))
   end
 
   it "renders attributes in <p>" do
     render
-    expect(rendered).to match(/Name/)
-    expect(rendered).to match(/1/)
-    expect(rendered).to match(/2/)
-    expect(rendered).to match(/Garden Type/)
-    expect(rendered).to match(//)
+    expect(rendered).to match(/#{@garden.name}/)
+    expect(rendered).to match(/#{@garden.square_feet}/)
+    expect(rendered).to match(/#{@garden.zone}/)
+    expect(rendered).to match(/#{@garden.garden_type}/)
+    expect(rendered).to match(/[#{@garden.user.email}]/)
   end
 end
