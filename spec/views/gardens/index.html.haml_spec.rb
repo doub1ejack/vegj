705aaee06cfec84c7d963c06697df217db054ba2
Micah <micah.mutrux@eatingwell.com>
2016-10-18 07:41:14 -0400

    SPEC: corrected test for garden listing page

--- spec/views/gardens/index.html.haml_spec.rb
+++ spec/views/gardens/index.html.haml_spec.rb
@@ -4,28 +4,35 @@ RSpec.describe "gardens/index", type: :view do
   before(:each) do
     assign(:gardens, [
       Garden.create!(
-        :name => "Name",
+        :name => "Test Name",
         :square_feet => 1,
         :zone => 2,
         :garden_type => "Garden Type",
-        :user => nil
+        :user => FactoryGirl.create(:user, email: "test1@example.com")
       ),
       Garden.create!(
-        :name => "Name",
+        :name => "Test Name",
         :square_feet => 1,
         :zone => 2,
         :garden_type => "Garden Type",
-        :user => nil
+        :user => FactoryGirl.create(:user, email: "test2@example.com")
       )
     ])
+    
+    # assign(:gardens, 
+      # [ 
+        # FactoryGirl.create(:garden, user_args: {email: "test1@test.com"}), 
+        # FactoryGirl.create(:garden, user_args: {email: "test2@test.com"}) 
+      # ]
+    # )
   end
 
   it "renders a list of gardens" do
     render
-    assert_select "tr>td", :text => "Name".to_s, :count => 2
+    assert_select "tr>td", :text => "Test Name", :count => 2
     assert_select "tr>td", :text => 1.to_s, :count => 2
     assert_select "tr>td", :text => 2.to_s, :count => 2
-    assert_select "tr>td", :text => "Garden Type".to_s, :count => 2
-    assert_select "tr>td", :text => nil.to_s, :count => 2
+    assert_select "tr>td", :text => "Garden Type", :count => 2
+    assert_select "tr>td", :text => /@example\.com/, :count => 2
   end
 end
