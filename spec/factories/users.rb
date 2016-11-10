65b78dee5b227ae04888140aca17a694bb701c75
Micah <micah.mutrux@eatingwell.com>
2016-11-10 08:16:54 -0500

    SPEC DASHBOARD: fixed feature tests for dashboard
    Now testing that authenticated users see dashboard, and
    unauthenticated users see welcome page.

--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -2,7 +2,12 @@ require 'ffaker'
 
 FactoryGirl.define do
   factory :user do |u|
-    u.email { |me| "#{FFaker::Name.name.gsub(/\s/,'.')}_#{rand(1000).to_s}@testing.com" }
+    u.name FFaker::Name.name
+    u.email { |me| "#{name.to_s.gsub(/\s/,'.')}_#{rand(1000).to_s}@testing.com" }
     u.password "password"
+    
+    factory :confirmed_user do |u|
+      u.password_confirmation "password"
+    end
   end
 end
--- spec/features/dashboard_spec.rb
+++ spec/features/dashboard_spec.rb
@@ -1,34 +1,33 @@
 require 'rails_helper'
+require 'factory_girl_rails'
 
-describe 'site administrator' do
+describe 'authenticated user' do
   it "accesses the dashboard" do
-    User.create(
-      email: 'user@example.com',
-      password: 'secret',
-      password_confirmation: 'secret'
-    )
+    user = FactoryGirl.build(:confirmed_user)
+    user.skip_confirmation!
+    user.save!
   
     visit root_path
     click_link 'Login'
-    fill_in 'Email', with: 'user@example.com'
-    fill_in 'Password', with: 'secret'
+    fill_in 'Email', with: user.email
+    fill_in 'Password', with: user.password
     click_button 'Log in'
     
-    current_path.should eq dashboard_path
-    within 'h1' do
-      page.should have_content 'Dashboard'
-    end
-    page.should have_content 'New Journal Entry'
-    page.should have_content 'Manage Plants'
-    page.should have_content 'Manage Gardens'
-  end
-  
-  it "is denied access when not logged in" do
-    visit dashboard_path
+    puts User.all.inspect
+    
+    expect(current_path).to eq(root_path)
 
-    current_path.should eq login_path
-    within 'h1' do
-      page.should have_content 'Log in'
-    end
+    expect(page).to have_content 'Welcome to your Dashboard'
+    expect(page).to have_content 'Your Journal'
+    expect(page).to have_content 'New Journal Entry'
+    expect(page).to have_content 'Your Plants'
+    expect(page).to have_content 'Your Gardens'
+  end
+end
+
+describe 'unauthenticated user' do
+  it "sees welcome page at site root" do
+    visit root_path
+    expect(page).to have_content 'Register to get started'   
   end
 end
