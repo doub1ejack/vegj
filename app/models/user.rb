1cf5204810dc873a90063cf726d0fead28f4b9a6
Micah <micah.mutrux@eatingwell.com>
2016-11-11 07:48:11 -0500

    SPEC USER: require name

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,4 +5,6 @@ class User < ActiveRecord::Base
          :recoverable, :rememberable, :trackable, 
          :validatable, :confirmable, :lockable
   has_many :gardens
+  
+  validates :name, presence: true
 end
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -3,26 +3,23 @@ require 'rails_helper'
 # first factorygirl test following http://everydayrails.com/2012/03/19/testing-series-rspec-models-factory-girl.html
 RSpec.describe User, type: :model do
   
-  before(:each) do
-    @fg = FactoryGirl
-  end
-  
   context "User Properties" do
   
     it "has a valid factory" do
-      user = @fg.build(:user) 
+      user = FactoryGirl.build(:user) 
       expect( user ).to be_valid
     end
     
-    it "does not require a name" do
-      user_without_name = @fg.build(:user, name: nil) 
-      expect( user_without_name ).to be_valid
+    it "requires a name" do
+      user_without_name = FactoryGirl.build(:user, name: nil) 
+      expect( user_without_name ).to_not be_valid
     end
   
     it "requires an email" do 
-      user_without_email = @fg.build(:user, email: nil) 
+      user_without_email = FactoryGirl.build(:user, email: nil) 
       expect( user_without_email ).to_not be_valid
     end
+
 
   end 
   
