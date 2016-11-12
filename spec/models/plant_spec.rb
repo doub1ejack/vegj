50c247432cc487d431de88d6a25955b589600542
Micah <micah.mutrux@eatingwell.com>
2016-11-12 09:25:09 -0500

    SPEC PLANT: testing model

--- spec/models/plant_spec.rb
+++ spec/models/plant_spec.rb
@@ -1,5 +1,18 @@
 require 'rails_helper'
 
 RSpec.describe Plant, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  
+  context "Plant Properties" do
+  
+    it "has a valid factory" do 
+      expect( FactoryGirl.build(:plant) ).to be_valid
+    end
+    
+    it "requires a common name" do
+      plant_without_name = FactoryGirl.build(:user, name: nil) 
+      expect( plant_without_name ).to_not be_valid
+    end
+  end 
+  
+  pending "should not belong to gardens (remove garden_id and create a mapping between plants and user gardens)"
 end
