48fab8daf7ac7054fe7dc07b01461e383ce2af4b
Micah <micah.mutrux@eatingwell.com>
2016-11-12 09:55:03 -0500

    RSPEC PLANT: updated controller tests
    Decided to update the model to have a method that returns an invalid
    plant model.  This way the invalid models should all be defined n a
    single place and when I update the model I'm more likely to remember
    to update the invalid properties.
    
    Also this was my first look at (amazingly) RDoc formatting.  While
    looking around I came across YARD formatting too, which seems more
    informative.  I haven't added the yard gem yet, but I think I'll
    try to get in the habbit of using yard sytax.
    
    see: http://rdoc.sourceforge.net/doc/index.html for rdoc info
    see: http://www.rubydoc.info/gems/yard/file/docs/GettingStarted.md for yard

--- app/models/plant.rb
+++ app/models/plant.rb
@@ -1,3 +1,5 @@
+require "factory_girl_rails"
+
 class Plant < ActiveRecord::Base
     
   belongs_to :garden
@@ -7,4 +9,14 @@ class Plant < ActiveRecord::Base
   enum sow_method: [ :direct, :indoor, :direct_indoor ]
   
   validates :garden, presence: true
+  
+  # Define invalid properties here instead of in the model test
+  # @return [Plant Model] an invalid plant model
+  def Plant.get_invalid_plant
+    invalid_properties = {
+      name: nil,
+      garden: nil
+    }
+    FactoryGirl.build(:plant, invalid_properties)
+  end
 end
