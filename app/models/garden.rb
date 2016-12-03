a16622836ee561a38f7a0bd9a5dd8b1ea5dfd8ed
Micah <micah.mutrux@eatingwell.com>
2016-12-03 13:55:18 -0500

    IMAGES: dynamic img for garden type
    If the garden is a veggie garden, we see the farm image.  Otherwise
    we see the flower garden image.

--- app/models/garden.rb
+++ app/models/garden.rb
@@ -5,5 +5,14 @@ class Garden < ActiveRecord::Base
   validates :name, presence: true
   validates :zone, presence: true
   validates :user, presence: true
+
+  attr_writer :thumb
+  def thumb
+    if(garden_type == "vegetable")
+      @thumb = '/assets/veggie-garden_16x9.jpg'
+    else
+      @thumb = '/assets/flower-garden_16x9.jpg'
+    end
+  end
   
 end
