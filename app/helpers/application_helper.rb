04f11bc6cb249c67fe7b4e3f2111dd013f6a4833
Micah <micah.mutrux@eatingwell.com>
2016-12-30 19:13:03 -0500

    GARDENS_PLANTS: BS styling for flash message
    This solved a couple problems.
    
      1. I couldn't see the message I'm tying to improve very well. It's
      easier now that it's fixed in the browser window.
    
      2. The flash message is no longer limited to :notice messages.
    
    I now need to make this bootstrap alert fade away politely and then
    fix the persistent double-flash message.

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -4,4 +4,18 @@ def header(text)
   content_for(:header) { text.to_s }
 end
 
+# Returns the propper bootstrap alert classes for each type of flash message.
+# @see For method usage: https://coderwall.com/p/jzofog/ruby-on-rails-flash-messages-with-bootstrap
+# @see For custom flash types: http://blog.remarkablelabs.com/2012/12/register-your-own-flash-types-rails-4-countdown-to-2013
+#
+# @param level [String] The flash type (eg :notice, :alert, etc)
+def get_bootstrap_alert_classes(level)
+  case level
+    when 'success' then 'alert alert-success'
+    when 'notice' then 'alert alert-info'
+    when 'alert' then 'alert alert-warning'
+    when 'error' then 'alert alert-error'
+  end
+end
+
 end
